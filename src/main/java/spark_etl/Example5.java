f2c498e626b66de106ec709053919b0494967948
Coley Shi <shikenian@126.com>
2018-06-29 14:24:01 +0800

    Example5     新增通过手动实现Aggregation类的方式来提供一些常用的Aggregation操作。     增加对Untyped User-Defined Aggregate Functions 相关方法的解读和描述

--- src/main/java/spark_etl/Example5.java
+++ src/main/java/spark_etl/Example5.java
@@ -1,9 +1,36 @@
 /**
  * 测试Spark相关的聚集函数相关的操作。
+ *
+ * 当前类测试的是非类型化的用户定义的聚合函数，也就是Untyped User-Defined Aggregate Functions
+ *
  * 聚集函数就是我们在SQL语言中对应的Max(),Sum() 等一些针对分组情况下的一些操作函数。
  * Spark本身支持的聚集函数很多，英语中叫做Aggregation
  * <p>
  * 有： sum(),count(), avg(),max(),min() 等等
+ * 手动实现对UserDefinedAggregateFunction的继承来实现对Aggregation操作的个性化实现。
+ * 需要实现以下几个方法：
+ *
+ * inputSchema()
+ *  为调用的方法返回一个StructType，告诉我们调用这个Aggregation的时候传递几个参数进来，参数的类型是什么样子的。
+ *  i.e. 比如myAverage 这个Aggregation，在调用的时候需要传递进去一个int类型的age信息，看下面的代码这个StructType其实就一个int类型的fields
+ *
+ * bufferSchema()
+ *  告诉调用的上层代码，这个Aggregation对象里面的buffer里面有几个column，并且每个column都是什么类型的。
+ *  buffer是这个类计算的中间对象，用来计算并且保存计算的结果。最终会提供给evaluate方法来得到最终的结果。
+ * dataType()
+ *  告诉上层调用的代码，这个Aggregation最终会返回什么类型的值，一般来说，最终这个Aggregation只会返回一个值，无论你传递进来多少，最终指挥返回一个结果。
+ *  而DataType可以告诉我们最终计算的结果是什么类型的。这个是和我们的evaluate方法是对应的，当然如果evaluate方法返回的结果是Object的类型的话，应该最终会被Cast成这个Datatype
+ * deterministic()
+ *  这个参数还不是很清楚作用，但是目前来看应该是一个开关，告诉Spark在同样的输入结果下，得到的输出结果应该是一致的。但是我不了解的是什么情况下得到的结果才能够不一致。
+ * initialize()
+ *  用来初始化Buffer，这个Buffer就是用来存储计算的中间变量的。
+ * merge()
+ *  个人猜测是Spark在计算的时候可能是并行的一种操作，因此的话可能有多个线程，每个线程都持有一个Buffer，但是最终所有并行的结果都要合并到一起做一个汇总，那么这个时候就会调用merge方法。
+ *  窄这个merge方法里面我们可以定义一个合并的规则，比如是不是相同就舍弃等等，或者是累加等等。注意，最终计算的结果会被放到第一个参数引用的对象中。这是需要特别注意的地方。
+ * evaluate()
+ *  取出Buffer中的值，做自己想要的计算得到想要的结果。只会执行一次
+ *
+ *
  */
 
 
